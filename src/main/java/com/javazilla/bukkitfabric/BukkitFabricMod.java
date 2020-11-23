640041be18872a2ffbbd2c4bb871db650aa3a48c
IsaiahPatton <isaiahpatton1@gmail.com>
2020-11-22 21:38:55 -0500

    Fix vanilla commands & Save bukkit.yml
    # Conflicts:
    #	src/main/java/com/javazilla/bukkitfabric/BukkitFabricMod.java

--- src/main/java/com/javazilla/bukkitfabric/BukkitFabricMod.java
+++ src/main/java/com/javazilla/bukkitfabric/BukkitFabricMod.java
@@ -21,41 +21,41 @@ package com.javazilla.bukkitfabric;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.function.Function;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
-import org.cardboardpowered.ingot.IngotReader;
+import org.cardboardpowered.library.Library;
+import org.cardboardpowered.library.LibraryKey;
 import org.cardboardpowered.library.LibraryManager;
 
+import com.google.common.collect.ImmutableMap;
 import com.javazilla.bukkitfabric.nms.MappingsReader;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.network.ServerLoginNetworkHandler;
 
 @SuppressWarnings("deprecation")
 public class BukkitFabricMod implements ModInitializer {
 
-    public static Logger LOGGER = BukkitLogger.getLogger();
+    public static Logger LOGGER = BukkitLogger.getLogger(); 
     public static boolean isAfterWorldLoad = false;
     public static final Random random = new Random();
 
     public static List<ServerLoginNetworkHandler> NETWORK_CASHE = new ArrayList<>();
 
-    @SuppressWarnings("deprecation")
     @Override
     public void onInitialize() {
-        LOGGER.info("");
-        LOGGER.info("Cardboard (formerly Bukkit4Fabric) Mod - CardboardPowered.org");
-        LOGGER.info("Version: 1.17-snapshot-ea");
-        LOGGER.info("");
+        LOGGER.info("Bukkit for Fabric Mod - Javazilla.com");
 
         loadLibs();
 
         try {
             MappingsReader.main(null);
-           //if (FabricLoader.getInstance().isDevelopmentEnvironment())
-           //     IngotReader.start();
+            //if (FabricLoader.getInstance().isDevelopmentEnvironment())
+            //    IngotReader.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -64,7 +64,12 @@ public class BukkitFabricMod implements ModInitializer {
 
     public static void loadLibs() {
         String repository = "https://repo.glowstone.net/repository/maven-public/";
-        new LibraryManager(repository, "lib", true, 2, CompatibilityBundle.CRAFTBUKKIT.libraries.values()).run();
+
+        Map<LibraryKey, Library> libraries = Stream.of(
+                new Library("org.xerial", "sqlite-jdbc", "3.21.0.1", LibraryManager.HashAlgorithm.SHA1, "81a0bcda2f100dc91dc402554f60ed2f696cded5"),
+                new Library("mysql", "mysql-connector-java", "5.1.46", LibraryManager.HashAlgorithm.SHA1, "9a3e63b387e376364211e96827bc27db8d7a92e9")
+            ).collect(ImmutableMap.toImmutableMap(Library::getLibraryKey, Function.identity()));
+        new LibraryManager(repository, "lib", true, 2, libraries.values()).run();
     }
 
 }
